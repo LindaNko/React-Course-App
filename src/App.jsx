d9a66c53c2254b0b5480ee19a11d6fe82603be92
Linda Nkosi <lindako63@outlook.com>
2026-02-21 07:59:30 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+ const name= 'Link';
+ const age = 20;
+
+  return (
+    <div>
+      <h1>Welcome to React I am {name}</h1>
+      <p>This is a simple React application.</p>
+      <p>Age: {age}</p>
+    </div>
+  )
+}
+
+export default App
